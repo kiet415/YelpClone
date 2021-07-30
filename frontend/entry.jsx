a7825ea40b8c0ca4b030e4ed68f99e22e04ea22e
Kiet Nguyen <kiet9657@yahoo.com>
2021-07-30 16:27:58 -0700

    Added session_api_util

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { signup, login, logout } from './util/session_api_util'
+//import configureStore from './store/store'
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  //example line
+  //const store = configureStore();
+  window.signup = signup
+  window.login = login
+  window.logout = logout
   ReactDOM.render(<h1>Welcome to Kelp</h1>, root);
 });
