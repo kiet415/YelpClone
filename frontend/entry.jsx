d0ad38709414e1719cc5a41321f2dcf504db3691
Kiet Nguyen <kiet9657@yahoo.com>
2021-07-30 14:25:11 -0700

    Add user auth comment

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  
+  //example line
   ReactDOM.render(<h1>Welcome to Kelp</h1>, root);
 });
