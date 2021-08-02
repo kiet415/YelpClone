4eb5a122a7273dcc18e1259841a4c8fec1a1700a
Kiet Nguyen <kiet9657@yahoo.com>
2021-08-01 23:06:39 -0700

    Added store and into entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { signup, login, logout } from './util/session_api_util'
-//import configureStore from './store/store'
+import configureStore from './store/store'
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  //const store = configureStore();
+  const store = configureStore();
   window.signup = signup
   window.login = login
   window.logout = logout
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; 
   ReactDOM.render(<h1>Welcome to Kelp</h1>, root);
 });
